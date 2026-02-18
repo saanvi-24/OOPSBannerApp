36025aa3d4098e3f91a5733e2b8f964ea27cbe63
saanvi-24 <saanvisonar2024@gmail.com>
2026-02-18 14:45:00 +0530

    Coded UC3 to Print OOPS to Console

---
+++ OOPSString.java
@@ -0,0 +1,10 @@
+public class UC2_PrintOOPS {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("*****"," ","*****"," ","**** "," ","*****"));
+        System.out.println(String.join("*   *"," ","*   *"," ","*   *"," ","*    "));
+        System.out.println(String.join("*   *"," ","*   *"," ","**** "," ","*****"));
+        System.out.println(String.join("*   *"," ","*   *"," ","*    "," ","    *"));
+        System.out.println(String.join("*****"," ","*****"," ","*    "," ","*****"));
+    }
+}
